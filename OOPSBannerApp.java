3d6b275948359ea402e58b9323edca27f49930cf
rr9919-code <rr9919@srmist.edu.in>
2026-03-05 14:31:14 +0530

    adding file

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,24 @@
 /**
  * OOPS Banner App
- * UC2 - Print OOPS in Banner Format
+ * UC3 - Print OOPS Banner using String.join() 
  * @author Rohith M
- * @version 2.0
+ * @version 3.0
  */
 
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
 
-        System.out.println("   ***   " + "   ***   " + " ******  " + " ***** ");
-        System.out.println("  **  ** " + "  **  ** " + " **   ** " + " **    ");
-        System.out.println(" **    **" + " **    **" + " **   ** " + " **    ");
-        System.out.println(" **    **" + " **    **" + " ******  " + "  ***  ");
-        System.out.println(" **    **" + " **    **" + " **      " + "    ** ");
-        System.out.println("  **  ** " + "  **  ** " + " **      " + "    ** ");
-        System.out.println("   ***   " + "   ***   " + " **      " + " ***** ");
+
+        System.out.println("OOPS");
+
+        System.out.println(String.join("   ***  ","    ***   ","  ******  ","   *****  ");
+        System.out.println(String.join("  **  ** ","  **  ** ","  **   **  "," **      ");
+        System.out.println(String.join(" **    ** ","**    ** "," **   **  "," **      ");
+        System.out.println(String.join(" **    ** ","**    **  ","******     "," ***   ");
+        System.out.println(String.join(" **    ** ","**    **  ","**           ","  ** ");
+        System.out.println(String.join("  **  **   ","**  **   ","**           ","  ** ");
+        System.out.println(String.join("   ***      ","***     ","**        "," *****  ");
+
 
     }
 }
