7d5f0462f9e55b0bda1e43f7892fbb464335aa24
rr9919-code <rr9919@srmist.edu.in>
2026-03-05 13:48:34 +0530

    Adding file

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+
+/**
+ * OOPS Banner App
+ * UC1 - Print OOPS 
+ * @author Rohith M
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+    }
+}
